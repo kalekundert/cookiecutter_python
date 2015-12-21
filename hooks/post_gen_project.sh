da02fc74ee85293ac3873223f8b0b71495e992f9
Kale Kundert <kale@thekunderts.net>
2015-12-21 09:43:52 -0800

    Don't install Sphinx automatically.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -11,6 +11,6 @@ git commit -m "Initial commit."
 # Create a virtual environment and install the new library in it.
 
 virtualenv -p python3 env
-env/bin/pip install bumpversion know_its_ok sphinx
+env/bin/pip install bumpversion know_its_ok
 env/bin/pip install -e .
 
