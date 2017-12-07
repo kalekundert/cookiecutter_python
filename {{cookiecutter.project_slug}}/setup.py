99ae8a4d6350ddde11117c429e41aa6df2c6e7ce
Kale Kundert <kale@thekunderts.net>
2017-12-07 10:35:46 -0800

    Simplify how the version in handled.
    Basically rely more on bumpversion to get rid of the fragile code that
    went looking for ``__version__`` with a regex.

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
-import re
-with open('{{ cookiecutter.project_slug }}/__init__.py') as file:
-    version_pattern = re.compile("__version__ = '(.*)'")
-    version = version_pattern.search(file.read()).group(1)
 with open('README.rst') as file:
     readme = file.read()
 
 setup(
     name='{{ cookiecutter.project_slug }}',
-    version=version,
+    version='0.0.0',
     author='{{ cookiecutter.full_name }}',
     author_email='{{ cookiecutter.email }}',
     description='{{ cookiecutter.project_short_description }}',
