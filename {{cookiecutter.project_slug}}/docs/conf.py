f2f30e389e8fd6e40ab5bc94801fc1eb024949d1
Kale Kundert <kale@thekunderts.net>
2020-12-01 22:18:54 -0500

    Fix the HTML static path.

--- {{cookiecutter.project_slug}}/docs/conf.py
+++ {{cookiecutter.project_slug}}/docs/conf.py
@@ -12,6 +12,7 @@ master_doc = 'index'
 source_suffix = '.rst'
 templates_path = ['_templates']
 exclude_patterns = ['_build']
+html_static_path = ['_static']
 default_role = 'any'
 
 ## Extensions
@@ -32,6 +33,5 @@ autodoc_default_options = {
         'exclude-members': '__dict__,__weakref__,__module__',
 }
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['static']
 pygments_style = 'sphinx'
 
